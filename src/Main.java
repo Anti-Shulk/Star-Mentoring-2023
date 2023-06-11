9805b92936078ef091de8e1cde0888e4d77484f9
Justin <84753002+Anti-Shulk@users.noreply.github.com>
2023-06-11 21:48:12 +0000

    created break/continue example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while (true) {
+            System.out.print("Please enter your input: ");
+            input = scanner.nextLine();
+
+            if (input.toLowerCase().equals("quit"))
+                break;
+            
+            if (input.toLowerCase().equals("pass"))
+                continue;
+
+            System.out.println("Your input was " + input);
+        }
+
+        scanner.close();
     }
 }
