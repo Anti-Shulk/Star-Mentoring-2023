66c5bdfe19306be17ea967a0c3eba1fec6f43e5b
Justin <84753002+Anti-Shulk@users.noreply.github.com>
2023-06-23 22:12:34 +0000

    functions

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,7 @@ public class Main {
         float trackWidth = 0;
 
         while (true) {
-            String input = scanner.nextLine().trim();
-            try {
-                trackWidth = Float.parseFloat(input);
-            } catch (NumberFormatException ignored) {
-                System.out.println("Input was not a valid floating point number");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
+            trackWidth = getFloat();
 
             if (trackWidth > TRACK_WIDTH_MAX || trackWidth < TRACK_WIDTH_MIN) {
                 System.out.println("Track width was not between " + 
@@ -34,95 +27,19 @@ public class Main {
 
         System.out.println("Please enter the velocity of the Front Left Wheel");
         System.out.print("between " + VELOCITY_MIN + " and " + VELOCITY_MAX + " inches per second: ");
-        float frontLeftVelocity = 0;
-
-        while (true) {
-            String input = scanner.nextLine().trim();
-            try {
-                frontLeftVelocity = Float.parseFloat(input);
-            } catch (NumberFormatException ignored) {
-                System.out.println("Input was not a valid floating point number");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
-
-            if (frontLeftVelocity > VELOCITY_MAX || frontLeftVelocity < VELOCITY_MIN) {
-                System.out.println("Track width was not between " + 
-                VELOCITY_MIN + " and " + VELOCITY_MAX + " inches");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
-            break;
-        }
+        float frontLeftVelocity = getVelocityInchesPerSecond();
 
         System.out.println("Please enter the velocity of the Front Right Wheel");
         System.out.print("between " + VELOCITY_MIN + " and " + VELOCITY_MAX + " inches per second: ");
-        float frontRightVelocity = 0;
-
-        while (true) {
-            String input = scanner.nextLine().trim();
-            try {
-                frontRightVelocity = Float.parseFloat(input);
-            } catch (NumberFormatException ignored) {
-                System.out.println("Input was not a valid floating point number");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
-
-            if (frontRightVelocity > VELOCITY_MAX || frontRightVelocity < VELOCITY_MIN) {
-                System.out.println("Track width was not between " + 
-                VELOCITY_MIN + " and " + VELOCITY_MAX + " inches");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
-            break;
-        }
+        float frontRightVelocity = getVelocityInchesPerSecond();
 
         System.out.println("Please enter the velocity of the Back Left Wheel");
         System.out.print("between " + VELOCITY_MIN + " and " + VELOCITY_MAX + " inches per second: ");
-        float backLeftVelocity = 0;
-
-        while (true) {
-            String input = scanner.nextLine().trim();
-            try {
-                backLeftVelocity = Float.parseFloat(input);
-            } catch (NumberFormatException ignored) {
-                System.out.println("Input was not a valid floating point number");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
-
-            if (backLeftVelocity > VELOCITY_MAX || backLeftVelocity < VELOCITY_MIN) {
-                System.out.println("Track width was not between " + 
-                VELOCITY_MIN + " and " + VELOCITY_MAX + " inches");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
-            break;
-        }
+        float backLeftVelocity = getVelocityInchesPerSecond();
 
         System.out.println("Please enter the velocity of the Back Right Wheel");
         System.out.print("between " + VELOCITY_MIN + " and " + VELOCITY_MAX + " inches per second: ");
-        float backRightVelocity = 0;
-
-        while (true) {
-            String input = scanner.nextLine().trim();
-            try {
-                backRightVelocity = Float.parseFloat(input);
-            } catch (NumberFormatException ignored) {
-                System.out.println("Input was not a valid floating point number");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
-
-            if (backRightVelocity > VELOCITY_MAX || backRightVelocity < VELOCITY_MIN) {
-                System.out.println("Track width was not between " + 
-                VELOCITY_MIN + " and " + VELOCITY_MAX + " inches");
-                System.out.print("Please reenter the input: ");
-                continue;
-            }
-            break;
-        }
+        float backRightVelocity = getVelocityInchesPerSecond();
 
         float angularVelocityRadians = (backRightVelocity + frontRightVelocity - frontLeftVelocity - backLeftVelocity) /
                                         (4 * trackWidth);
@@ -132,5 +49,43 @@ public class Main {
         System.out.println("The angular velocity is " + angularVelocityDegrees + " degrees per second");
 
         scanner.close();
+    }
+
+    static float getFloat() {
+        Scanner scanner = new Scanner(System.in);
+        float number = 0;
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+            try {
+                number = Float.parseFloat(input);
+            } catch (NumberFormatException ignored) {
+                System.out.println("Input was not a valid floating point number");
+                System.out.print("Please reenter the input: ");
+                continue;
+            }
+            break;
+        }
+
+        return number;
+    }
+
+    static float getVelocityInchesPerSecond() {
+        final float VELOCITY_MIN = -100;
+        final float VELOCITY_MAX = 100;
+
+        float velocity = 0;
+
+        while (true) {
+            velocity = getFloat();
+            if (velocity > VELOCITY_MAX || velocity < VELOCITY_MIN) {
+                System.out.println("Velocity was not between " + 
+                VELOCITY_MIN + " and " + VELOCITY_MAX + " inches");
+                System.out.print("Please reenter the input: ");
+                continue;
+            }
+            break;
+        }
+        return velocity;
     }
 }
